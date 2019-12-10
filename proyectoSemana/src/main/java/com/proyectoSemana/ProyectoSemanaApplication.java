cd32a1cbe9cde7b553922acca83fcde824f1e7a1
Evelyn Correa <evelyn._.correa@hotmail.com>
2019-12-10 18:23:04 -0300

    initial commit

---
+++ proyectoSemana/src/main/java/com/proyectoSemana/ProyectoSemanaApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoSemana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSemanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSemanaApplication.class, args);
+	}
+
+}
---
+++ proyectoSemana/src/test/java/com/proyectoSemana/ProyectoSemanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoSemana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSemanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
