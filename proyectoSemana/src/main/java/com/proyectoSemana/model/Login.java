738eec9b1a9a78eab87a9f90b5da5d65c453fded
ZorD007 <zordlegand@gmail.com>
2019-12-11 13:51:21 -0300

    wena cabros supuestamente llegan los packages

---
+++ proyectoSemana/src/main/java/com/proyectoSemana/model/Login.java
@@ -0,0 +1,42 @@
+package com.proyectoSemana.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "login")
+public class Login {
+
+    @Id
+    @Column(name = "id_login")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ proyectoSemana/src/main/java/com/proyectoSemana/repository/LoginRepository.java
@@ -0,0 +1,6 @@
+package com.proyectoSemana.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LoginRepository extends CrudRepository {
+}
